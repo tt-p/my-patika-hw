14d20f2fe1c51d28663a38b4942d46105d936e5b
tt-p <thapinarli26@gmail.com>
2022-03-02 13:11:14 +0300

    change indent

--- odev1.sql
+++ odev1.sql
@@ -2,16 +2,16 @@
  * P_1: Film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
  * 
  * P_2: Film tablosunda bulunan tüm sütunlardaki verileri
- *		film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
+ *      film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
  * 
  * P_3: Film tablosunda bulunan tüm sütunlardaki verileri
- *     rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
+ *      rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
  * 
  * P_4: Customer tablosunda bulunan 
- *		first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
+ *      first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
  * 
  * P_5: Film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP
- *		aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
+ *      aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
  * 
  * */
 
