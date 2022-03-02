6d4a53f6d2b2f358a14df970b4ea5c802f013253
tt-p <thapinarli26@gmail.com>
2022-03-02 19:46:32 +0300

    add odev10 solution

---
+++ odev10.sql
@@ -0,0 +1,39 @@
+/*
+ * P_1: city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) 
+ *      isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+ * 
+ * P_2: customer tablosu ile payment tablosunda bulunan payment_id ile 
+ *      customer tablosundaki first_name ve last_name isimlerini 
+ *      birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+ * 
+ * P_3: customer tablosu ile rental tablosunda bulunan rental_id ile
+ *      customer tablosundaki first_name ve last_name isimlerini
+ *      birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+ * 
+ * */
+
+-- S_1:
+select 
+	ci.city,
+	co.country
+from city ci
+left join
+	country co on ci.country_id = co.country_id;
+
+-- S_2:
+select
+	p.payment_id,
+	c.first_name,
+	c.last_name
+from customer c
+right join
+	payment p on c.customer_id = p.customer_id;
+
+-- S_3:
+select
+	r.rental_id,
+	c.first_name,
+	c.last_name
+from customer c
+full join
+	rental r on c.customer_id = r.customer_id;
